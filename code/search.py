7c4863340f2b26935012d97914704c8bcefe4ea9
AdrianoSantino <adrianosantinof@gmail.com>
2024-05-22 21:45:35 -0300

    tfidf polish

--- code/search.py
+++ code/search.py
@@ -3,21 +3,19 @@ from pickles import *
 
 
 def search(text):
-    word_lists = create_word_lists_from_texts([text])[0]
-
     db_tf_list = pickle_load("tf_list")
     db_main_empty_vec = pickle_load("main_empty_vec")
+    word_lists = create_word_lists_from_texts([text])[0]
 
     for word in word_lists:
-        if word not in db_main_empty_vec:
-            for tf in db_tf_list:
-                tf[word] = 0
-            db_main_empty_vec[word] = 0
+        for tf in db_tf_list:
+            tf[word] = tf.get(word, 0)
+        db_main_empty_vec[word] = db_main_empty_vec.get(word, 0)
 
-    tf_list = calculate_term_frequencies([word_lists], db_main_empty_vec)
-    db_tf_list += tf_list
+    search_tf = calculate_term_frequencies([word_lists], db_main_empty_vec)
+    db_tf_list += search_tf
 
-    idf = calculate_inverse_document_frequencies(db_main_empty_vec, db_tf_list, len(tf_list) + 1)
+    idf = calculate_inverse_document_frequencies(db_main_empty_vec, db_tf_list, len(search_tf) + 1)
     tfidf_vec_list = calculate_tfidf_vectors(db_tf_list, idf)
     pass
 
