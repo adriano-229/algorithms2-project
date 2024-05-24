e2e547cd997a107e99cee43bb8475e08ea1fa810
AdrianoSantino <adrianosantinof@gmail.com>
2024-05-24 15:17:55 -0300

    reorganized and started the answer formatting.

--- source/create.py
+++ source/create.py
@@ -1,40 +1,43 @@
 from categorization.parsing import *
 from pickles import *
 
-FILENAME_DB_TF_LIST = "DB_TF_LIST"
-FILENAME_DB_MAIN_EMPTY_VEC = "DB_MAIN_EMPTY_VEC"
+DB_FILENAMES = "DB_FILENAMES"
+DB_TF_LIST = "DB_TF_LIST"
+DB_MAIN_EMPTY_VEC = "DB_MAIN_EMPTY_VEC"
+
+
+# def create_db():
+#     try:
+#         open(os.path.join(PATH, DB_FILENAMES))
+#         open(os.path.join(PATH, DB_MAIN_EMPTY_VEC))
+#         if not bool(input("DB exists, rewrite DB? Press 1 YES or ENTER NO: ")):
+#             print("Finished")
+#             return
+#     except:
+#         if not bool(input("No existing DB, want to create? Press 1 YES or ENTER NO: ")):
+#             print("No db created, quiting")
+#             return
+#         else:
+#             pass
 
 
 def create(path):
     if not os.path.isdir(path):
         raise Exception("The provided path is not a directory")
 
-    # try:
-    #     open(os.path.join(PATH, FILENAME_DB_TF_LIST))
-    #     open(os.path.join(PATH, FILENAME_DB_MAIN_EMPTY_VEC))
-    #     if not bool(input("DB exists, rewrite existing DB? Press 1 YES or ENTER NO: ")):
-    #         print("Finished")
-    #         return
-    # except:
-    #     if not bool(input("No existing DB, want to create? Press 1 YES or ENTER NO: ")):
-    #         print("No db created, quiting")
-    #         return
-    #     else:
-    #         pass
+    # create_db()
 
     corpus_pdfs_str = create_texts_from_pdfs(path)
-    filenames = corpus_pdfs_str.keys()  # TODO, asociar nombres a resultados
+    filenames, texts = list(corpus_pdfs_str.keys()), corpus_pdfs_str.values()
 
-    word_lists = create_word_lists_from_texts(corpus_pdfs_str.values())
+    word_lists = create_word_lists_from_texts(texts)
     main_empty_vec = create_main_vector(word_lists)
     tf_list = calculate_term_frequencies(word_lists, main_empty_vec)
+    del path, corpus_pdfs_str, word_lists
 
-    # idf = calculate_inverse_document_frequencies(main_empty_vec, tf_list, len(corpus_pdfs_str))
-    # tfidf_vec_list = calculate_tfidf_vectors(tf_list, idf)
-    # print(cosine_similarity(list(tfidf_vec_list[2].values()), list(tfidf_vec_list[1].values())))
-
-    pickle_dump(tf_list, FILENAME_DB_TF_LIST)
-    pickle_dump(main_empty_vec, FILENAME_DB_MAIN_EMPTY_VEC)
+    pickle_dump(filenames, DB_FILENAMES)
+    pickle_dump(tf_list, DB_TF_LIST)
+    pickle_dump(main_empty_vec, DB_MAIN_EMPTY_VEC)
     print("document data-base created successfully")
 
 
--- source/search.py
+++ source/search.py
@@ -1,22 +1,45 @@
 from categorization.parsing import *
+from create import DB_FILENAMES, DB_TF_LIST, DB_MAIN_EMPTY_VEC
 from pickles import *
 
 
+def remove_keys(dic_list):
+    lst = []
+    for dic in dic_list:
+        lst.append(list(dic.values()))
+    return lst
+
+
+def associate_names(names_lst, ele_lst):
+    dic = {}
+    if len(names_lst) != len(ele_lst):
+        raise Exception("associate_names error!")
+
+    for i in range(len(names_lst)):
+        dic[names_lst[i]] = ele_lst[i]
+    return dic
+
+
 def search(text):
-    DB_TF_LIST = pickle_load("FILENAME_DB_TF_LIST")
-    DB_MAIN_EMPTY_VEC = pickle_load("FILENAME_DB_MAIN_EMPTY_VEC")
+    filenames = pickle_load(DB_FILENAMES)
+    tf_list = pickle_load(DB_TF_LIST)
+    main_empty_vec = pickle_load(DB_MAIN_EMPTY_VEC)
+    corpus_size = len(filenames) + 1
+
     word_lists = create_word_lists_from_texts([text])[0]
-
     for word in word_lists:
-        for tf in DB_TF_LIST:
+        for tf in tf_list:
             tf[word] = tf.get(word, 0)
-        DB_MAIN_EMPTY_VEC[word] = DB_MAIN_EMPTY_VEC.get(word, 0)
+        main_empty_vec[word] = main_empty_vec.get(word, 0)
 
-    search_tf = calculate_term_frequencies([word_lists], DB_MAIN_EMPTY_VEC)
-    DB_TF_LIST += search_tf
+    search_tf = calculate_term_frequencies([word_lists], main_empty_vec)
+    tf_list += search_tf
+    idf = calculate_inverse_document_frequencies(main_empty_vec, tf_list, corpus_size)
+    tfidf_vec_list = calculate_tfidf_vectors(tf_list, idf)
+    tfidf_vec_list = remove_keys(tfidf_vec_list)
 
-    idf = calculate_inverse_document_frequencies(DB_MAIN_EMPTY_VEC, DB_TF_LIST, len(search_tf) + 1)
-    tfidf_vec_list = calculate_tfidf_vectors(DB_TF_LIST, idf)
+    # tfidf_vec_list = associate_names(filenames, tfidf_vec_list) TODO, terminar procesamiento coseno + asociación ans
+
     pass
 
 
