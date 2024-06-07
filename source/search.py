1da543df7e101a3fac05cf67fa88fa79f788806e
AdrianoSantino <adrianosantinof@gmail.com>
2024-06-06 23:02:09 -0300

    v2.0

--- source/search.py
+++ source/search.py
@@ -14,15 +14,16 @@ def search(text):
     db_filenames = pickle_load(DB_FILENAMES)
     db_tf_list = pickle_load(DB_TF_LIST)
     db_idf = pickle_load(DB_IDF)
+    corpus_size = len(db_tf_list) + 1
 
     word_list = create_word_lists_from_texts([text])
     search_tf = calculate_term_frequencies(word_list)
     db_tf_list += search_tf
 
     for word in word_list[0]:
-        db_idf[word] = db_idf.get(word, 0) + 1
+        db_idf[word] = db_idf.get(word, 1) + 1
     for word in db_idf.keys():
-        db_idf[word] = math.log2(len(db_tf_list) / db_idf[word])
+        db_idf[word] = math.log2(corpus_size / db_idf[word])
 
     tfidf = calculate_tfidf_vectors(db_tf_list, db_idf)
     search_vector = tfidf.pop()
@@ -36,4 +37,7 @@ def search(text):
 
 
 if __name__ == "__main__":
-    search("""""")
+    search("""
+    La Mixteca Alta Oaxaqueña, México (MAO), presenta niveles
+moderados a graves de degradación de suelo.
+    """)
