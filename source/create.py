fded3c46162a9a6fa8dba54edf994b0e93c2447e
AdrianoSantino <adrianosantinof@gmail.com>
2024-06-06 23:02:09 -0300

    v2.0

---
+++ source/create.py
@@ -0,0 +1,29 @@
+from parsing import *
+from pickles_ import *
+
+
+def create(path):
+    if not os.path.isdir(path):
+        raise Exception("The provided path is not a directory")
+
+    corpus_pdfs_str = create_texts_from_pdfs(path)
+    filenames, texts = corpus_pdfs_str.keys(), corpus_pdfs_str.values()
+
+    word_lists = create_word_lists_from_texts(texts)
+    tf_list = calculate_term_frequencies(word_lists)
+    idf = calculate_document_occurrences(word_lists)
+
+    dump = {
+        DB_FILENAMES: list(filenames),
+        DB_TF_LIST: tf_list,
+        DB_IDF: idf
+    }
+    for name, content in dump.items():
+        pickle_dump(content, name)
+
+    print("document data-base created successfully")
+    return
+
+
+if __name__ == "__main__":
+    create("/home/admin1/Documents/Universidad/2do/Algoritmos2/proyecto-algo2/pdfs")
